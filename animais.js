5aeddc66cf624908e2d057b8d1b5bce82ca9b475
NicolasLemosRibeiro <nicolaslemosribeiro04@gmail.com>
2023-10-27 02:10:07 -0300

    adicionando um arquivo

---
+++ animais.js
@@ -0,0 +1,7 @@
+const animals = ["tucano-pinposo", "peixe-boi", "avestruz-feio", "papagaio-chorão", "golfinho-saliente", "leão"];
+if(!animals.includes("leão")){
+    console.log("Leão não existe no array animals.");
+}
+else{
+    console.log("Existe um leão no array animals.");
+}
---
+++ series.js
@@ -0,0 +1,5 @@
+const my3FavoriteTVShows = ["Arrow", "Pokemon", "Naruto"];
+console.log(my3FavoriteTVShows);
+
+const itens = my3FavoriteTVShows.length === 3;
+console.log(itens);
